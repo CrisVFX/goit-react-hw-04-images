870039e14125c7035e5e883216b142e65c005e5b
Cris <132308930+CrisVFX@users.noreply.github.com>
2024-05-02 22:41:49 +0800

    commit

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '40924863-f555801e4729fc82d13bdd65e';
+const API_KEY = '41346783-54974ba518e4200dea29991c0';
 
 export const getAPI = async (query, page) => {
   const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
